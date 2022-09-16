7891f4de946e78e56edb757b497ca6a1112d54ed
Abrar <amahi2001@gmail.com>
2022-09-16 09:47:36 -0400

    using TS inline templates

--- src/app/nested/nested.component.ts
+++ src/app/nested/nested.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nested',
-  templateUrl: './nested.component.html',
-  styleUrls: ['./nested.component.css']
+  // templateUrl: './nested.component.html',
+  template: `
+    <p>This is nested inline html</p>
+    <app-server></app-server>
+  `,
+  styleUrls: ['./nested.component.css'],
 })
 export class NestedComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
