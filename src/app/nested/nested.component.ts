b0792a66a124a577c98a52ce51bd7cf52afd33f4
Abrar <amahi2001@gmail.com>
2022-09-16 11:58:49 -0400

    editing selecotr by attribute

--- src/app/nested/nested.component.ts
+++ src/app/nested/nested.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nested',
+  selector: '[app-nested]',
   templateUrl: './nested.component.html',
   // template: `
   //   <h3>This component contains a nested component</h3>
