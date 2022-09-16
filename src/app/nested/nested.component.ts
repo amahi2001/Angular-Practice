df9a70259f59705a84c30a15e2908ded5ebc3293
Abrar <amahi2001@gmail.com>
2022-09-16 11:53:02 -0400

    **ignore** just some styling

--- src/app/nested/nested.component.ts
+++ src/app/nested/nested.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nested',
-  // templateUrl: './nested.component.html',
-  template: `
-    <p>This is nested inline html</p>
-    <app-server></app-server>
-  `,
+  templateUrl: './nested.component.html',
+  // template: `
+  //   <h3>This component contains a nested component</h3>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./nested.component.css'],
 })
 export class NestedComponent implements OnInit {
