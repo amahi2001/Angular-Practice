c2e0de411902bda26fc596add451948ef662ac62
Abrar Mahi <amahi2001@gmail.com>
2022-09-19 14:37:42 -0400

    directive ngStyle to dynamically update element styles (also property binding)

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,7 +11,16 @@ export class ServerComponent {
     server_id : number = 12;
     server_status : string = 'offline';
 
+
+    constructor(){
+        this.server_status = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus(){
         return this.server_status;
+    }
+
+    getColor(){
+        return this.server_status==='online' ? 'green' : 'red'
     }
 }
