6526e81e98b8f6501c28b5be509c52cc8ac16ec2
Abrar <amahi2001@gmail.com>
2022-09-21 09:55:06 -0400

    rendering recipes from recipe-list and other HTML changes

--- src/app/recipes/recipe-lists/recipe-lists.component.ts
+++ src/app/recipes/recipe-lists/recipe-lists.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-lists',
   templateUrl: './recipe-lists.component.html',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListsComponent implements OnInit {
 
-  constructor() { }
+  recipe_list: Recipe[] = [new Recipe('Gyro', 'test', 'https://www.thespruceeats.com/thmb/YPq6QiaRiQbpiV-DexGfDwn6AoQ=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/gyros-a-greek-classic-912899-hero-01-6aabc80d1fb9440d85c51dd0b2fb9d50.jpg'), new Recipe('Gyro', 'test', 'https://www.thespruceeats.com/thmb/YPq6QiaRiQbpiV-DexGfDwn6AoQ=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/gyros-a-greek-classic-912899-hero-01-6aabc80d1fb9440d85c51dd0b2fb9d50.jpg')];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
